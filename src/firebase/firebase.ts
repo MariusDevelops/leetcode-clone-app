189760cc0bd78b0dc5821521e7e6580873c9c6a4
MariusDevelops <brazasmarius@gmail.com>
2023-06-01 17:01:40 +0300

    feat: firebase setup

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLICK_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLICK_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLICK_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLICK_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLICK_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLICK_APP_ID,
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+const fireStore = getFirestore(app);
+
+export { auth, fireStore, app };
